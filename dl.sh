7f26c3f27da87986944a5ca09d5ab0449f2d070b
ceastld <1950946176@qq.com>
2025-05-06 14:59:07 +0000

    fix(env create)

--- dl.sh
+++ dl.sh
@@ -1,3 +1,2 @@
 # Download demo data from Google Drive and move to MONONPHM_DATA_TRACKING
 gdown https://drive.google.com/drive/folders/1XHHabTt_IgYPmGZj0Gj1dyTm7dwyGEvb --folder
-mv FFHQ_* $MONONPHM_DATA_TRACKING/
